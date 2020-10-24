webosose/chromium68

[backport][needs_review][agl] Sent the proper PID to IVI
REVIEW
- needs surface_id in widget.h? webos_widget.cc/.h is missing
- changes to WebAppWindow and WebAppWindowBase were partially ported

Make IVI match the PID properly, setting it at the right time.

Change-Id: I6ebc20297dcbdd4874f5eb7badc8673c48becfff
Reviewed-by: Jose Dapena <jose.dapena@lge.com>
Tested-by: Jose Dapena <jose.dapena@lge.com>

(cherry-picked from
https://github.com/webosose/chromium53/commit/e17b3f27d9044154dc61d600cc6ed898fd22e5de)

src/webos/webapp_window.h
@@ -29,7 +29,7 @@ class WebAppWindowDelegate;
 
 class WebAppWindow : public app_runtime::WebAppWindow {
  public:
-  WebAppWindow(const app_runtime::WebAppWindowBase::CreateParams& params);
+  WebAppWindow(const app_runtime::WebAppWindowBase::CreateParams& params, int surface_id);
   ~WebAppWindow() override;
 
   void SetDelegate(WebAppWindowDelegate* webapp_window_delegate);

Igalia/chromium79

src/webos/webapp_window.h
@@ -29,7 +29,7 @@ class WebAppWindowDelegate;
 
 class WebAppWindow : public neva_app_runtime::WebAppWindow {
  public:
-  WebAppWindow(const neva_app_runtime::WebAppWindow::CreateParams& params);
+  WebAppWindow(const neva_app_runtime::WebAppWindow::CreateParams& params, int surface_id);
   ~WebAppWindow() override;
 
   void SetDelegate(WebAppWindowDelegate* webapp_window_delegate);